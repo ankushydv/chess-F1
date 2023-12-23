59c6cb7589d0965ea1b8515bd85848bdaf6dfd51
ankush kumar <ankush@gmail.com>
2023-12-23 22:59:28 +0530

    add pieces css and file

---
+++ src/Component/Pieces/Pieces.js
@@ -0,0 +1,23 @@
+import "./Pieces.css";
+
+const Pieces = () => {
+  const positions = Array(8)
+    .fill("")
+    .map((x) => Array(8).fill(""));
+
+  positions[0][0] = "wr";
+  positions[7][7] = "br";
+
+  console.log(positions);
+  return (
+    <div className="pieces">
+      {positions.map((x, rank) =>
+        x.map((f, file) =>
+          positions[rank][file] ? positions[rank][file] : null
+        )
+      )}
+    </div>
+  );
+};
+
+export default Pieces;
