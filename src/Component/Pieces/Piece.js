0a17e5d2f5fc4e882c194719d5c5d2f8a529da1b
ankush kumar <ankush@gmail.com>
2023-12-24 22:51:25 +0530

    add draggable

--- src/Component/Pieces/Piece.js
+++ src/Component/Pieces/Piece.js
@@ -1,5 +1,5 @@
 const Piece = ({ file, piece, rank }) => {
-  return <div className={`piece ${piece} p-${rank}${file}`} />;
+  return <div className={`piece ${piece} p-${rank}${file}`} draggable={true} />;
 };
 
 export default Piece;
