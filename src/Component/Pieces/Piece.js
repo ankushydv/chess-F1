59a884ce8f410ac738451d4ea27e6cb001f9dcb7
ankush kumar <ankush@gmail.com>
2023-12-24 23:26:52 +0530

    added some feat of drag func into pieces

--- src/Component/Pieces/Piece.js
+++ src/Component/Pieces/Piece.js
@@ -1,5 +1,14 @@
 const Piece = ({ file, piece, rank }) => {
-  return <div className={`piece ${piece} p-${rank}${file}`} draggable={true} />;
+  const onDragStart = (e) => {
+    e.dataTransfer.setData("text/plain", `${piece}, ${rank}, ${file}`);
+  };
+  return (
+    <div
+      className={`piece ${piece} p-${rank}${file}`}
+      draggable={true}
+      onDragStart={onDragStart}
+    />
+  );
 };
 
 export default Piece;
--- src/Component/Pieces/Pieces.js
+++ src/Component/Pieces/Pieces.js
@@ -6,8 +6,16 @@ import { createPosition } from "../../helper";
 const Pieces = () => {
   const [state, setState] = useState(createPosition);
 
+  const onDrop = (e) => {
+    console.log(e.dataTransfer.getData("text"));
+  };
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="pieces">
+    <div className="pieces" onDrop={onDrop} onDragOver={onDragOver}>
       {state.map((x, rank) =>
         x.map((f, file) =>
           state[rank][file] ? (
