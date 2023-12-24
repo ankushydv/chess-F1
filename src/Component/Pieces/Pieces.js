7f375261ca64f1a9ff2bffd959a86893fe7a6ef4
ankush kumar <ankush@gmail.com>
2023-12-24 23:53:24 +0530

    get cordinate of pieces

--- src/Component/Pieces/Pieces.js
+++ src/Component/Pieces/Pieces.js
@@ -1,12 +1,22 @@
 import Piece from "./Piece";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Pieces.css";
 import { createPosition } from "../../helper";
 
 const Pieces = () => {
+  const ref = useRef();
   const [state, setState] = useState(createPosition);
+  const getCordinates = (e) => {
+    const { top, left, width } = ref.current.getBoundingClientRect();
+    const size = width / 8;
+    const y = Math.floor((e.clientY - left) / size);
+    const x = 7 - Math.floor((e.clientX - top) / size);
+    return { x, y };
+  };
 
   const onDrop = (e) => {
+    const { x, y } = getCordinates(e);
+    console.log(x, y);
     const [piece, rank, file] = e.dataTransfer.getData("text").split(",");
     console.log(piece, rank, file);
   };
@@ -16,7 +26,7 @@ const Pieces = () => {
   };
 
   return (
-    <div className="pieces" onDrop={onDrop} onDragOver={onDragOver}>
+    <div className="pieces" onDrop={onDrop} ref={ref} onDragOver={onDragOver}>
       {state.map((x, rank) =>
         x.map((f, file) =>
           state[rank][file] ? (
