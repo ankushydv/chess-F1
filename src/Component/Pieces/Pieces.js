7b39e65518d8d2bf5a04e30972c2195f689d9baa
ankush kumar <ankush@gmail.com>
2023-12-27 08:53:09 +0530

    position set always at last move :currrentnewPositions: 💯

--- src/Component/Pieces/Pieces.js
+++ src/Component/Pieces/Pieces.js
@@ -1,16 +1,16 @@
 import Piece from "./Piece";
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import "./Pieces.css";
-import { createPosition, copyPosition } from "../../helper";
-import { useAppContext } from "../../Context/Context";
+import { copyPosition } from "../../helper";
+import { useAppContext } from "../../context/Context";
 import { makeNewMove } from "../../Reducer/action/move";
 
 const Pieces = () => {
   const ref = useRef();
   const { AppState, dispatch } = useAppContext();
   console.log("AppState", AppState);
-  const currentPosition = AppState.positions;
-  // const [state, setState] = useState(createPosition);
+  const currentPosition = AppState.positions[AppState.positions.length - 1];
+  console.log("CurrentPosition", currentPosition);
   const getCordinates = (e) => {
     const { top, left, width } = ref.current.getBoundingClientRect();
     const size = width / 8;
@@ -22,13 +22,13 @@ const Pieces = () => {
   const onDrop = (e) => {
     let newPositions = copyPosition(currentPosition);
     const { x, y } = getCordinates(e);
-    console.log(x, y);
+    // console.log(x, y);
     const [piece, rank, file] = e.dataTransfer.getData("text").split(",");
     let rankNumber = Number(rank);
     let fileNumber = Number(file);
     newPositions[rankNumber][fileNumber] = "";
     newPositions[x][y] = piece;
-    console.log(newPositions);
+    // console.log(newPositions);
     dispatch(makeNewMove({ newPositions }));
   };
 
