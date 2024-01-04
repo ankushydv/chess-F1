13ca6b4ccbb4a49a79294db828d73dd872a87335
ankush kumar <ankush@gmail.com>
2024-01-04 16:29:00 +0530

    add genrate move reducer

--- src/Component/Pieces/Piece.js
+++ src/Component/Pieces/Piece.js
@@ -1,10 +1,29 @@
+import { useAppContext } from "../../context/Context";
+import arbiter from "../../arbiter/arbiter";
+import { genrateCandidateMoves } from "../../Reducer/action/move";
+
 const Piece = ({ file, piece, rank }) => {
+  const { AppState, dispatch } = useAppContext();
+  const { turn, positions } = AppState;
+  //export as context for current position
+  const currentPosition = positions[positions.length - 1];
+
   const onDragStart = (e) => {
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text/plain", `${piece}, ${rank}, ${file}`);
     setTimeout(() => {
       e.target.style.display = "none";
     }, 0);
+    if (turn === piece[0]) {
+      const candidateMoves = arbiter.getRegularMoves({
+        position: currentPosition,
+        piece,
+        rank,
+        file,
+      });
+      console.log("candidate move", candidateMoves);
+      dispatch(genrateCandidateMoves({ candidateMoves }));
+    }
   };
 
   const onDragEnd = (e) => {
