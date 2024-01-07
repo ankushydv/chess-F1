d998239d853bca4dbf3c070a9612c0bd82214b2f
ankush kumar <ankush@gmail.com>
2024-01-07 22:16:34 +0530

    added bishops moves

--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -48,3 +48,33 @@ export const getKnightMoves = ({ position, rank, file }) => {
   });
   return moves;
 };
+
+export const getBishopMoves = ({ position, piece, rank, file }) => {
+  const moves = [];
+  const us = piece[0];
+  const enemy = us === "w" ? "b" : "w";
+  const direaction = [
+    [-1, 1],
+    [1, 1],
+    [1, -1],
+    [-1, -1],
+  ];
+  direaction.forEach((dir) => {
+    for (let i = 1; i <= 8; i++) {
+      const x = rank + i * dir[0];
+      const y = file + i * dir[1];
+      if (position?.[x]?.[y] === undefined) break;
+
+      if (position[x][y].startsWith(enemy)) {
+        moves.push([x, y]);
+        break;
+      }
+      if (position[x][y].startsWith(us)) {
+        break;
+      }
+      moves.push([x, y]);
+    }
+  });
+  // console.log(moves[moves.length - 1]);
+  return moves;
+};
