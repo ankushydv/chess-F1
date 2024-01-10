59334f9b0bf3e9ebaa4b277367cba47123050355
ankush kumar <ankush@gmail.com>
2024-01-10 21:58:07 +0530

    added there cell value where pawn promotion

--- src/Component/Popup/PromotionBox/Promotion.js
+++ src/Component/Popup/PromotionBox/Promotion.js
@@ -1,24 +1,26 @@
+import { useAppContext } from "../../../context";
 import "./Promotion.css";
 const Promotion = () => {
+  const { AppState } = useAppContext();
+  const { promotionSquar, status } = AppState;
+  console.log("prom", promotionSquar, status);
   const options = ["q", "r", "b", "n"];
   const color = "b";
-  const x = 1;
-  const y = 1;
   const getPromotionBoxPosition = () => {
     let style = {};
 
-    if (x === 7) {
+    if (promotionSquar.x === 7) {
       style.top = "-12.5%";
     } else {
       style.top = "97.5%";
     }
 
-    if (y <= 1) {
+    if (promotionSquar.y <= 1) {
       style.left = "0%";
-    } else if (y >= 5) {
+    } else if (promotionSquar.y >= 5) {
       style.right = "0%";
     } else {
-      style.left = `${12.5 * y - 20}%`;
+      style.left = `${12.5 * promotionSquar.y - 20}%`;
     }
 
     return style;
