904ff79634fa7d8189680f8126765edacea1753a
ankush kumar <ankush@gmail.com>
2024-02-19 14:00:19 +0530

    feature of king check implemented

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -7,6 +7,8 @@ import {
   getPawnMoves,
   getPawnCapture,
   getCastlingMoves,
+  getPieces,
+  getKingPosition,
 } from "./getMoves";
 import { movePawn, movePiece } from "./move";
 
@@ -43,6 +45,7 @@ const arbiter = {
     file,
   }) {
     let moves = this.getRegularMoves({ position, piece, rank, file });
+    const notInCheckMoves = [];
     if (piece.endsWith("p")) {
       moves = [
         ...moves,
@@ -60,13 +63,46 @@ const arbiter = {
           file,
         }),
       ];
-      console.log("position:" , position )
-      moves.forEach(([x,y]) => {
-        const positionAfterMove = this.performMoves({ positions:position, piece , rank, file, x, y});
-        console.log(positionAfterMove)
-      })
+     
     }
-    return moves;
+    moves.forEach(([x,y]) => {
+
+      const positionAfterMove = this.performMoves({ positions:position, piece , rank, file, x, y});
+      console.log("positionAfterMove" , positionAfterMove)
+      if(!this.isPlayerInCheck({positionAfterMove,position, player:piece[0]})){
+          notInCheckMoves.push( [x,y] );
+      }
+    })
+    return notInCheckMoves;
+  },
+  isPlayerInCheck: function ({positionAfterMove, position , player}){
+    const enemy = player.startsWith("w") ? "b" : "w";
+    let kingPos = getKingPosition(positionAfterMove, player);
+    const enemyPieces = getPieces(positionAfterMove, enemy);
+    const enemyMoves = enemyPieces.reduce(
+      (acc, p) =>
+        (acc = [
+          ...acc,
+          ...(p.piece.endsWith("p")
+            ? getPawnCapture({
+                position: positionAfterMove,
+                prevPosition: position,
+                ...p,
+              })
+            : this.getRegularMoves({
+                position: positionAfterMove,
+                ...p,
+              })),
+        ]),
+      []
+    );
+
+    if (enemyMoves.some(([x, y]) => kingPos[0] === x && kingPos[1] === y)){
+      console.log("is come")
+      return true;
+    }
+    else return false;
+
   },
   performMoves: function ({ positions, piece, rank, file, x, y }) {
     if (piece.endsWith("p")) {
